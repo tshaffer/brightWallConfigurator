0ca5721b9f77d75c6482669e554c529f94a3585c
Ted Shaffer <ted@brightsign.biz>
2021-03-18 07:25:06 -0700

    add code to poll for BrightSign devices

--- src/controller/brightSign.ts
+++ src/controller/brightSign.ts
@@ -2,13 +2,30 @@ import { isNil } from 'lodash';
 import { addBrightSign } from '../model';
 import { BrightSignConfig } from '../type';
 
+let pollForBrightSignsTimer: ReturnType<typeof setTimeout>;
+
 export const launchApp = () => {
   return ((dispatch: any): any => {
     getBrightSignConfig()
       .then((brightSignConfig: BrightSignConfig) => {
+
+        // add the BrightSign that the custom device web page is running on - is this correct? appropriate? ok?
         dispatch(addBrightSign(brightSignConfig.brightSignAttributes.serialNumber, brightSignConfig.brightSignAttributes.isBrightWall));
+
+        // start timer to get list of BrightSigns in the wall
+        pollForBrightSignsTimer = setInterval(getBrightWallDeviceList, 1000, dispatch);
       });
   });
+};
+
+const getBrightWallDeviceList = (dispatch: any) => {
+  console.log('getBrightWallDeviceList invoked');
+  fetch('/GetBrightWallDeviceList')
+    .then(response => response.json())
+    .then((brightSignDeviceList: any) => {
+      console.log('response from GetBrightWallDeviceList');
+      console.log(brightSignDeviceList);
+    });
 };
 
 export const getBrightSignConfig = (): Promise<BrightSignConfig> => {
