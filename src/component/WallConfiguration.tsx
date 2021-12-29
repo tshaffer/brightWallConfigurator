9ca0f76948f8f013bac23f553398cf9159c8547e
Ted Shaffer <ted@brightsign.biz>
2021-12-29 05:34:26 -0800

    Support for reentering configurator.

--- src/component/WallConfiguration.tsx
+++ src/component/WallConfiguration.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
@@ -6,12 +9,62 @@ import '../styles/configurator.css';
 
 import DeviceList from './DeviceList';
 import ScreensInWall from './ScreensInWall';
+import { exitConfigurator, launchAlignmentTool, reenterConfigurator } from '../controller';
+import {
+  getBrightWallSetupScreenEnabled,
+} from '../selector';
+
+export interface WallConfigurationProps {
+  brightWallSetupScreenEnabled: boolean;
+  onReenterConfigurator: () => any;
+  onExitConfigurator: () => any;
+  onLaunchAlignmentTool: () => any;
+  onExitAlignmentTool: () => any;
+}
+
 
 // -----------------------------------------------------------------------
 // Component
 // -----------------------------------------------------------------------
 
-const WallConfiguration = () => {
+const WallConfiguration = (props: WallConfigurationProps) => {
+
+  const handleExitConfigurator = (event: any) => {
+    console.log('handleExitConfigurator invoked');
+    props.onExitConfigurator();
+  };
+
+  const handleReenterConfigurator = (event: any) => {
+    console.log('handleReenterConfigurator invoked');
+    props.onReenterConfigurator();
+  };
+
+  const handleLaunchAlignment = (event: any) => {
+    console.log('handleLaunchAlignment invoked');
+    props.onLaunchAlignmentTool();
+  };
+
+  const renderConfiguratorEntryExit = () => {
+    if (props.brightWallSetupScreenEnabled) {
+      return (
+        <div className='rightButtonContainer'>
+          <button onClick={handleExitConfigurator}>
+            Start Wall
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className='rightButtonContainer' >
+          <button onClick={handleReenterConfigurator}>
+            Reboot to reenter device setup
+          </button>
+        </div>
+      );
+    }
+  };
+
+  const configuratorEntryExit = renderConfiguratorEntryExit();
 
   return (
     <DndProvider backend={HTML5Backend}>
@@ -20,20 +73,32 @@ const WallConfiguration = () => {
         <ScreensInWall />
 
         <div className='leftButtonContainer'>
-          <button>
+          <button onClick={handleLaunchAlignment}>
             Test Alignment
           </button>
         </div>
 
-        <div className='rightButtonContainer'>
-          <button>
-            Start Wall
-          </button>
-        </div>
+        {configuratorEntryExit}
+
       </div>
     </DndProvider>
   );
 };
 
-export default WallConfiguration;
+function mapStateToProps(state: any): Partial<WallConfigurationProps> {
+  return {
+    brightWallSetupScreenEnabled: getBrightWallSetupScreenEnabled(state),
+  };
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return bindActionCreators({
+    onExitConfigurator: exitConfigurator,
+    onReenterConfigurator: reenterConfigurator,
+    onLaunchAlignmentTool: launchAlignmentTool,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WallConfiguration);
+
 
