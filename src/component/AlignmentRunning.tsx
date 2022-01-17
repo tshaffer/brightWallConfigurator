7f85ed9928e588518fb9390bc9552f30a585f66b
Ted Shaffer <ted@brightsign.biz>
2022-01-17 05:10:24 -0800

    Updated screens to re-enter configurator.

--- src/component/AlignmentRunning.tsx
+++ src/component/AlignmentRunning.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import { style } from 'typestyle';
+
 import { reenterConfigurator } from '../controller';
 
 export interface AlignmentRunningProps {
@@ -15,19 +17,42 @@ export interface AlignmentRunningProps {
 
 const AlignmentRunning = (props: AlignmentRunningProps) => {
 
+  const getDivStyle = () => {
+    return style({
+      margin: 0,
+      padding: 0,
+      display: 'block',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      width: '100%',
+      textAlign: 'center',
+    });
+  };
+
+  const getButtonStyle = () => {
+    return style({
+      padding: '15px',
+      backgroundColor: '#311c6b',
+      marginTop: '15px',
+      color: 'white',
+    });
+  };
+
   const handleReenterConfigurator = (event: any) => {
     console.log('handleReenterConfigurator invoked');
     props.onReenterConfigurator();
   };
 
-
   return (
-    <div>
-      <p>Video Wall Alignment is currently running. Would you like to stop it and re-enter configuration?</p>
-      <button onClick={handleReenterConfigurator}>
-        Enter Configuration
-      </button>
-
+    <div className={getDivStyle()}>
+      Video Wall Alignment is currently running. Would you like to stop it and re-enter configuration?
+      <br />
+      <button
+        className={getButtonStyle()}
+        onClick={handleReenterConfigurator}
+      >
+        Enter Configuration </button>
     </div>
   );
 };
--- src/component/PresentationRunning.tsx
+++ src/component/PresentationRunning.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { getActivePresentationName } from '../selector';
+
+import { style } from 'typestyle';
 
 import { reenterConfigurator } from '../controller';
+import { getActivePresentationName } from '../selector';
 
 export interface PresentationRunningProps {
   activePresentationName: string;
@@ -17,6 +19,29 @@ export interface PresentationRunningProps {
 
 const PresentationRunning = (props: PresentationRunningProps) => {
 
+  const getDivStyle = () => {
+    return style({
+      margin: 0,
+      padding: 0,
+      display: 'block',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      width: '100%',
+      textAlign: 'center',
+    });
+  };
+
+  const getButtonStyle = () => {
+    return style({
+      padding: '15px',
+      backgroundColor: '#311c6b',
+      marginTop: '15px',
+      color: 'white',
+    });
+  };
+
+
   const handleReenterConfigurator = (event: any) => {
     console.log('handleReenterConfigurator invoked');
     props.onReenterConfigurator();
@@ -24,9 +49,12 @@ const PresentationRunning = (props: PresentationRunningProps) => {
 
 
   return (
-    <div>
-      <p>{props.activePresentationName} is currently running. Would you like to stop it and re-enter configuration?</p>
-      <button onClick={handleReenterConfigurator}>
+    <div className={getDivStyle()}>
+      {props.activePresentationName} is currently running. Would you like to stop it and re-enter configuration?
+      <br />
+      <button
+        className={getButtonStyle()}
+        onClick={handleReenterConfigurator}>
         Enter Configuration
       </button>
 
